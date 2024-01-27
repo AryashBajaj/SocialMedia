97166ed3b9753e092f74441cfc4eacaa7e826561
AryashBajaj <aryashbajaj@gmail.com>
2024-01-27 18:47:27 +0530

    user groups, permissions and default groups

--- website/main/views.py
+++ website/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group, Permission
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from main.forms import RegisterForm, PostForm
 from .models import Post
 
@@ -9,12 +9,29 @@ from .models import Post
 @login_required(login_url = "/login")
 def home(request) :
     posts = Post.objects.all()
-
     if request.method == "POST" :
         post_id = request.POST.get("post-id")
-        post = posts.filter(id=post_id).first()
-        if post and post.author == request.user :
-            post.delete()
+        user_id = request.POST.get("user-id")
+
+        if post_id :
+            post = posts.filter(id=post_id).first()
+            if post and (post.author == request.user or request.user.has_perm("main.delete_post")) :
+                post.delete()
+        
+        if user_id :
+            user = User.objects.filter(id=user_id).first()
+            if user and request.user.is_staff :
+                try :
+                    group = Group.objects.get(name="default")
+                    group.user_set.remove(user)
+                except :
+                    pass
+
+                try :
+                    group = Group.objects.get(name="mod")
+                    group.user_set.remove(user)
+                except :
+                    pass
 
     return render(request, "main/home.html", {"posts" : posts})
 
@@ -30,6 +47,7 @@ def sign_up(request) :
     return render(request, "registration/sign_up.html", {"form" : form})
 
 @login_required(login_url="/login")
+@permission_required("main.add_post", login_url="/login", raise_exception=False)
 def create_post(request) :
     if request.method == "POST" :
         form = PostForm(request.POST)
