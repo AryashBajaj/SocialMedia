e51ce21670206372bb5c092ecfffb7d3aa2fe2a5
AryashBajaj <aryashbajaj@gmail.com>
2024-01-31 03:12:27 +0530

    database changes to include comments

--- website/main/models.py
+++ website/main/models.py
@@ -11,4 +11,13 @@ class Post(models.Model) :
 
     def __str__(self) :
         return self.title + "\n" + self.description
+    
+class Reply(models.Model) :
+    replyTo = models.ForeignKey(Post, on_delete = models.CASCADE)
+    commentAuthor = models.ForeignKey(User, on_delete = models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now= True)
 
+    def __str__(self) :
+        return self.content
